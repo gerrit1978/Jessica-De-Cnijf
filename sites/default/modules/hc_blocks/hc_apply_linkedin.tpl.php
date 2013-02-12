24f3b77913fef670380b97a656e49ae86d822981
Gerrit Vos <gerrit@localhost.localdomain>
2013-02-12 17:32:41 +0100

    HC Blocks module

---
+++ sites/default/modules/hc_blocks/hc_apply_linkedin.tpl.php
@@ -0,0 +1,10 @@
+<div class="button-linkedin" id="buttonLinkedin">   
+  <div class="linkedin-script"  id="linkedinScript">
+    <script src="http://platform.linkedin.com/in.js" type="text/javascript">api_key: uaeb3ij81pds </script>          
+    <script type="IN/Apply" data-companyname="Jessica De Cnijf" 
+            data-jobtitle="<?php print $jobtitle; ?>"
+            data-themecolor="#111" data-email="gerrit@hedgecomm.be">
+      </script>
+  </div>
+  <div class="linkedin-title">Apply with LinkedIn</div>
+</div>
