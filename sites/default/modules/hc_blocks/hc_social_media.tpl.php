5af0d4958c65f77d604fbdb9aadf5baec5675a54
Gerrit Vos <gerrit@localhost.localdomain>
2013-02-14 12:57:44 +0100

    Apply block

--- sites/default/modules/hc_blocks/hc_social_media.tpl.php
+++ sites/default/modules/hc_blocks/hc_social_media.tpl.php
@@ -9,8 +9,7 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script><div class="fb-like" data-href="http://jessica.local" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></li>
 <li class="tw"><a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
-<li class="li"><script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
-<script type="IN/Share" data-url="http://jessica.local" data-counter="right"></script></li>
+<li class="li"><script type="IN/Share" data-url="http://jessica.local" data-counter="right"></script></li>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></li>
 <!--<li class="gplus"><g:plusone size="medium" annotation="none"></g:plusone><script type="text/javascript">
   window.___gcfg = {lang: 'nl'};
