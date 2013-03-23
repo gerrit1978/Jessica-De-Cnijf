5d0e0a93fb15a6eeccea1a6ffaa5d20f866a25ea
Gerrit Vos <gevo27@gmail.com>
2013-03-23 17:38:34 +0100

    Theming finalization

--- sites/default/modules/hc_blocks/hc_apply_linkedin.tpl.php
+++ sites/default/modules/hc_blocks/hc_apply_linkedin.tpl.php
@@ -1,7 +1,7 @@
 <div class="button-linkedin" id="buttonLinkedin">   
   <div class="linkedin-script"  id="linkedinScript">
     <script src="http://platform.linkedin.com/in.js" type="text/javascript">api_key: uaeb3ij81pds </script>          
-    <script type="IN/Apply" data-companyname="Jessica De Cnijf" 
+    <script type="IN/Apply" data-companyname="Linx" 
             data-jobtitle="<?php print $jobtitle; ?>"
             data-themecolor="#111" data-email="gerrit@hedgecomm.be">
       </script>
